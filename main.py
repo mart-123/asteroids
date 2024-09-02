bcec31f18eff5fc78ca7874f59c2dcc8734a7d3e
mart-123 <torchbeam@gmail.com>
2024-09-02 17:41:43 +0100

    player motion added

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0  # delta time
+    dt = 0  # delta time in seconds
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        player.update(dt)
         screen.fill((0,0,0))
         player.draw(screen)
         pygame.display.flip()
