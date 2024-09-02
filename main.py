cd8573fab056a1e35e6640fcb48e78625dddab0f
mart-123 <torchbeam@gmail.com>
2024-09-02 17:21:25 +0100

    now drawing player on screen

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0  # delta time
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -13,7 +17,10 @@ def main():
                 return
             
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 # the pythonic way to prevent running if imported as a module
